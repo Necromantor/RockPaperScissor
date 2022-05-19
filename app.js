3c66982db298c3de52e1823fdb5110ff4676df5a
Necromantor <nelcostaa@hotmail.com>
2022-05-19 12:52:33 -0300

    Add User Interface & Modify js accordingly.

--- app.js
+++ app.js
@@ -1,67 +1,85 @@
-function computerPlay () {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
+const options = document.querySelectorAll('.options');
+const result = document.querySelector('#result');
+const playerScore = document.querySelector('#p-score');
+const computerScore = document.querySelector('#c-score');
+const reset = document.querySelector('.reset')
 
-    if (randomNumber == 1) {
-        return (computerChoice = "rock");
-    }
-    else if (randomNumber == 2) {
-        return (computerChoice = "paper");
-    }
-    else {
-        return (computerChoice = "scissors");
-    }
-}
-function playerPlay() {
-    return (playerChoice = prompt("Rock, Paper or Scissor?").toLowerCase())
-        
-}
+reset.addEventListener('click',() => location.reload());
 
+options.forEach((option) => {
+    option.addEventListener('click', playGame);
+});
 
-function playRound (playerChoice, computerChoice) {
-    if (playerChoice == "rock" && computerChoice == "paper") {
-        return (winner = "You lose! Paper beats Rock")
-    } else if (playerChoice == "paper" && computerChoice == "scissors") {
-        return (winner = "You lose! Scissors beats Paper")
-    } else if (playerChoice == "scissors" && computerChoice == "rock") {
-        return (winner = "You lose! Rock beats Scissors")
-    } else if (playerChoice == "paper" && computerChoice == "rock") {
-        return (winner = "You win! Paper beats Rock")
-    } else if (playerChoice == "rock" && computerChoice == "scissors") {
-        return (winner = "You win! Rock beats Scissors")
-    } else if (playerChoice == "scissors" && computerChoice == "paper") {
-        return (winner = "You win! Scissors beats Paper")
+function playGame() {
+    const playerChoice = this.textContent;
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+    const computerChoice = computerOptions[Math.floor(Math.random() * 3)]
+
+    playRound(playerChoice, computerChoice);
+    matchScore(playerScore, computerScore);
+    matchResult();
+};
+
+function playRound(playerChoice, computerChoice) {
+    const currentRound = `${playerChoice} vs ${computerChoice}`;
+    
+    if (playerChoice === "rock" && computerChoice === "paper") {
+        result.textContent = 
+        (`You lose! Paper beats Rock`);
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        result.textContent =  
+        (`You lose! Scissors beats Paper`);
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        result.textContent =  
+        (`You lose! Rock beats Scissors`);
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        result.textContent =  
+        (`You win! Paper beats Rock`);
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        result.textContent =
+        (`You win! Rock beats Scissors`);
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        result.textContent = 
+        (`You win! Scissors beats Paper`);
     } else {
-        return (winner = "It's a draw! Both picked the same")
+        result.textContent =  
+        (`It's a draw! Both picked the same.`);
     }
-}
+    
+};
 
-playerCounter = 0;
-computerCounter = 0;
+function matchScore(playerScore, computerScore) {
+     if (result.textContent === `You win! Paper beats Rock` || result.textContent === `You win! Scissors beats Paper` || result.textContent === `You win! Rock beats Scissors`){
+        ++playerScore.textContent;
+    } else if (result.textContent === "You lose! Rock beats Scissors" || result.textContent === "You lose! Scissors beats Paper" || result.textContent === "You lose! Paper beats Rock") {
+        ++computerScore.textContent;
+    } else {
+        ++playerScore.textContent;
+        ++computerScore.textContent;
+    }
+};
 
-function game () {
-    for (let i = 0; i < 5; i++) {
-        if (i <= 5) {
-        console.log(playRound(playerPlay(), computerPlay()));
-        console.log("player:", playerChoice);
-        console.log("computer:", computerChoice);
-        } if (winner == "You win! Paper beats Rock" || winner == "You win! Scissors beats Paper" || winner == "You win! Rock beats Scissors"){
-            ++playerCounter;
-            console.log("player Counter:", playerCounter);
-        } else if (winner == "You lose! Rock beats Scissors" || winner == "You lose! Scissors beats Paper" || winner == "You lose! Paper beats Rock") {
-            ++computerCounter;
-            console.log("computer counter: ", computerCounter);
-        };
+function matchResult() {
+    if (playerScore.textContent == 5 || computerScore.textContent == 5) {
+        if (playerScore.textContent == computerScore.textContent) {
+            result.textContent = `It's a draw! Lame...`; 
+            result.style.color = 'blue';
+            endGame();
+        } else if (playerScore.textContent > computerScore.textContent) {
+            result.textContent = `VI VON!!!`
+            result.style.color = 'green'
+            endGame();
+        } else if (playerScore.textContent < computerScore.textContent) {
+            result.textContent = `You lost! OMEGALUL`;
+            result.style.color = 'red';
+            endGame();
+        }
     };
-}
+};
 
-
-game();
-
-if (computerCounter > playerCounter){
-    console.log("Computer Wins!!!! u suck.");
-} else if (playerCounter > computerCounter){
-    console.log("Player Wins!!!!! nice... I guess.");
-} else {
-    console.log("It's a Draw! Lame.")
-}
-
+function endGame() {
+    options.forEach((option) => {
+        option.removeEventListener('click', playGame)
+    })
+};
